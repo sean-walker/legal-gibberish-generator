3b4c1a0394fe69a2fe5aaa0124e0891468c22b6b
sean-walker <sean_walker@outlook.com>
2017-05-05 23:04:21 -0400

    Fix file storage.

--- corpusParser.py
+++ corpusParser.py
@@ -18,9 +18,10 @@ class CorpusParser:
         delim = ' +++$+++ '
         corpus = 'sc.corpus'
         plain = 'sc.plain'
+        tokenized = 'sc.tok'
 
         # parse corpus into plaintext
-        if not os.path.exists(plain):
+        if not os.path.exists(os.path.join(APP_STATIC, plain)):
             print ('Parsing corpus to plaintext...', end='')
             with open(os.path.join(APP_STATIC, corpus), 'r') as f1, open(os.path.join(APP_STATIC, plain), 'w') as f2:
                 for line in f1:
@@ -32,13 +33,19 @@ class CorpusParser:
         else:
             print ('Found plaintext file {}'.format(plain))
 
-        print ('Tokenizing plaintext...', end='')
-        with open(os.path.join(APP_STATIC, plain), 'r') as f:
-            tokens = []
-            for line in f:
-                tokens += nltk.word_tokenize(line)
-        self.tokens = tokens
-        print ('done')
+        if not os.path.exists(os.path.join(APP_STATIC, tokenized)):
+            print ('Tokenizing plaintext...', end='')
+            with open(os.path.join(APP_STATIC, plain), 'r') as f:
+                tokens = []
+                for line in f:
+                    tokens += nltk.word_tokenize(line)
+            self.tokens = tokens
+            with open(os.path.join(APP_STATIC, tokenized), 'wb') as f:
+                pickle.dump(self.tokens, f)
+            print ('done')
+        else:
+            with open(os.path.join(APP_STATIC, tokenized), 'rb') as f:
+                self.tokens = pickle.load(f)
 
     def get_tokens(self):
         return self.tokens
